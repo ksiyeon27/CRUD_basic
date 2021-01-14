a3662dffb6895139164682791004aa6d6552a1b5
siyeon Kim <siyeonk27@gmail.com>
2021-01-14 12:32:52 +0900

    post_list Read

--- CRUD/posts/models.py
+++ CRUD/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='제목', max_length=100)
+    writer = models.CharField(verbose_name='글쓴이', max_length=50)
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+        # 정체성을 달아주는 느낌. 화면에 이걸로보이지. postobject~이런식이 아니라.
--- CRUD/posts/views.py
+++ CRUD/posts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Post
 # Create your views here.
 
 
 def post_list(request):
-    return HttpResponse('글 리스트')
+    # Read(R)
+    # 포스트들을 불러와서 리스트 형태로 보여준다
+    posts = Post.objects.all()
+
+    ctx = {'posts': posts}  # 딕셔너리 키; html로 넘어가는 스트링. 밸루; 객체개념.
+    # 렌더는 입히는 것. 어떤 형태로 보이게 만든다
+    return render(request, template_name='posts/list.html', context=ctx)
