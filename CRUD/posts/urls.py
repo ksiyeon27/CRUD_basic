df35cdf6d9de674402651158b46c9e4c25f82036
siyeon Kim <siyeonk27@gmail.com>
2021-01-14 14:03:30 +0900

    post_detail READ

--- CRUD/posts/urls.py
+++ CRUD/posts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', view=views.post_list, name='list'),  # url 이름이 list
-
+    path('<int:post_id>/', view=views.post_detail, name='detail')
 
 ]
--- CRUD/posts/views.py
+++ CRUD/posts/views.py
@@ -11,3 +11,13 @@ def post_list(request):
     ctx = {'posts': posts}  # 딕셔너리 키; html로 넘어가는 스트링. 밸루; 객체개념.
     # 렌더는 입히는 것. 어떤 형태로 보이게 만든다
     return render(request, template_name='posts/list.html', context=ctx)
+
+
+def post_detail(request, post_id):
+    # Read(R)
+    # 특정 포스트를 불러와서 상세정보를 보여준다
+
+    post = Post.objects.get(id=post_id)
+    ctx = {'post': post}
+
+    return render(request, template_name='posts/detail.html', context=ctx)
